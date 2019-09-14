1bf1ed87bf28721db495cf03d4e42c4eba799059
Zetrax <zabjd22@gmail.com>
2019-09-14 03:06:54 +0300

    Added Mainmenu with combobox for project list

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ import problem27_module as p27
 
 # Installing image utility
 from PIL import Image, ImageTk
-import Pmw
-class Home(tk.Tk):
+import Pmw # Python MegaWidget module
+
+class Home(tk.Tk): 
     def __init__(self, *kwargs, **args):
         tk.Tk.__init__(self, *kwargs, **args)
         
@@ -13,35 +14,68 @@ class Home(tk.Tk):
         container = tk.Frame(self)
         container.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
         
+        self.frames = {}
+        
+        for F in (MainPage, project_solution):
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky='nsew')
+        
+        self.frames[MainPage].populatelist(list(self.frames.keys()))
+        
+        self.showframe(MainPage)
+        
+    def showframe(self, frm):
+        print ('Showing Frame')
+        # removing all frames from grid
+        for f in self.frames.values():
+            f.grid_remove()
+        frame = self.frames[frm]
+        frame.grid(row=0, column=0, sticky='nsew')
+        frame.tkraise()
+
+class MainPage(tk.Frame):
+    selectedframe = None
+    frames = {}
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        
         self.logoimg = Image.open("./images/icon.jpg")
         self.renderimg = ImageTk.PhotoImage(self.logoimg)
         logo = tk.Label(self, image=self.renderimg, text="EulerProject")
-        logo.pack(side=tk.TOP, padx=40)
+        logo.pack(side=tk.TOP, padx=40, pady=(10, 0))
         
         # Contains all the controlls required to navigate projects
         controlframe = tk.Frame(self)
         controlframe.pack(side=tk.TOP, pady=(10, 30))
         
-        self.frames = {}
+        combobox = Pmw.ComboBox(controlframe, label_text='Projects', labelpos='wn',
+                        listbox_width=24, dropdown=1,
+                        selectioncommand=self.choseEntery,
+                        scrolledlist_items=self.frames)
+        combobox.pack(side=tk.TOP, fill=tk.BOTH, expand=True, padx=8, pady=8)
+        if len(self.frames) > 0:
+            combobox.selectitem(self.frames[0])
         
-        # Adding controlls to controlframe
-        projectlist = tk.Listbox(controlframe, width=20, height=3)
-        projectlist_scroller = tk.Scrollbar(controlframe, command=projectlist.yview)
-        projectlist.config(yscrollcommand=projectlist_scroller.set)
-        projectlist.pack(side=tk.LEFT)
-        projectlist_scroller.pack(side=tk.RIGHT, fill=tk.Y)
+        goBtn = tk.Button(controlframe, text='GO', command= lambda: controller.showframe(project_solution))
+        goBtn.pack(side=tk.TOP, fill=tk.BOTH)
         
+    def choseEntery(self, entry):
+        print('selected %s' % entry)
+        self.selectedframe = entry
         
-        
-        for i in range(10):
-            projectlist.insert(tk.END, i)
-        
-        self.frames = {}
-        
-    def showframe(self, frm):
-        frame = self.frames[frm]
-        frame.tkraise()
+    def populatelist(self, frames):
+        self.frames = frames
 
+class project_solution(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        
+        solve_btn = tk.Button(self, text='Solve', command=self.solve)
+        solve_btn.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+    
+    def solve(self):
+        pass
 
 app = Home()
 app.mainloop()
